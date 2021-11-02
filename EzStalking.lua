eb6451bc5c962c04098f63741b83bf11e9fcec26
Malte Höpfner <git@behoeppy.de>
2021-11-02 10:23:41 +0100

    Bumps API to Deadlands

--- EzStalking.lua
+++ EzStalking.lua
@@ -8,7 +8,7 @@ EzStalking.name         = 'EzStalking'
 EzStalking.title        = 'Easy Stalking'
 EzStalking.slash        = '/ezlog'
 EzStalking.author       = 'muh'
-EzStalking.version      = '1.4.3'
+EzStalking.version      = '1.4.4'
 EzStalking.var_version  = 2
 
 EzStalking.defaults = {
